0751adc4fb1044d60c2ad2c1726a7af1119acea9
Ryangrae Kim <dev@rrkim.com>
2024-05-28 12:17:46 +0900

    feat: captcha 추가

--- src/main/java/com/rrkim/frame/ProxyLoginFrame.java
+++ src/main/java/com/rrkim/frame/ProxyLoginFrame.java
@@ -1,15 +1,20 @@
 package com.rrkim.frame;
 
+import nl.captcha.Captcha;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class ProxyLoginFrame extends JFrame {
 
-    private final String PROXY_URL = "localhost";
+    private final String PROXY_URL = "ipcam-proxy.rrkim.com";
     private final String PROXY_PORT = "8081";
+    private final Captcha captcha = new Captcha.Builder(200, 50).addText().addBorder().addNoise().build();
+    private JTextField captchaTextField;
 
     public ProxyLoginFrame() {
         setTitle("Login Frame");
@@ -17,12 +22,14 @@ public class ProxyLoginFrame extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         JPanel buttonPanel = getButtonPanel();
+        JPanel captchaPanel = getCaptchaPanel();
 
         // 공간 추가
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 1));
-        panel.add(new JPanel());
-        panel.add(buttonPanel);
+        panel.setLayout(new BorderLayout());
+        panel.add(new JPanel(), BorderLayout.NORTH);
+        panel.add(captchaPanel, BorderLayout.CENTER);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
 
         add(panel);
         setVisible(true);
@@ -34,6 +41,12 @@ public class ProxyLoginFrame extends JFrame {
             JFileChooser fileChooser = new JFileChooser();
             int returnValue = fileChooser.showOpenDialog(null);
             if (returnValue == JFileChooser.APPROVE_OPTION) {
+                String captchaText = captchaTextField.getText();
+                if(captchaText == null || captchaText.isEmpty() || !captcha.isCorrect(captchaText)) {
+                    JOptionPane.showMessageDialog(null, "올바른 캡챠 코드를 입력하세요.");
+                    return;
+                }
+
                 File selectedFile = fileChooser.getSelectedFile();
                 loginProcess(selectedFile);
             }
@@ -42,6 +55,19 @@ public class ProxyLoginFrame extends JFrame {
         buttonPanel.add(selectFileButton);
 
         return buttonPanel;
+    }
+
+    private JPanel getCaptchaPanel() {
+        BufferedImage img = captcha.getImage();
+        JLabel captchaLabel = new JLabel(new ImageIcon(img));
+        captchaTextField = new JTextField(5);
+
+        JPanel captchaPanel = new JPanel();
+        captchaPanel.setLayout(new GridLayout(2,1));
+        captchaPanel.add(captchaLabel);
+        captchaPanel.add(captchaTextField);
+
+        return captchaPanel;
     }
 
     private void loginProcess(File talchwiFile){
